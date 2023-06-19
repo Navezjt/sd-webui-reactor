4ebf8427a50ec5c6ef61aa8c332e149ee46e75d2
Art Gourieff <85128026+Gourieff@users.noreply.github.com>
2023-06-19 17:00:48 +0700

    Small fix

--- scripts/roop_logging.py
+++ scripts/roop_logging.py
@@ -24,7 +24,7 @@ class ColoredFormatter(logging.Formatter):
 
 
 # Create a new logger
-logger = logging.getLogger("roop")
+logger = logging.getLogger("nsfw-roop")
 logger.propagate = False
 
 # Add handler if we don't have one.
