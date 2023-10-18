6be9162b1a04a2645c6122807c425a0c3ecf0e45
Gourieff <777@lovemet.ru>
2023-10-19 03:36:40 +0700

    FIX: Separate STATUS logger level

--- scripts/reactor_version.py
+++ scripts/reactor_version.py
@@ -6,5 +6,5 @@ from scripts.reactor_logger import logger, get_Run, set_Run
 is_run = get_Run()
 
 if not is_run:
-    logger.info(f"Running {version_flag}")
+    logger.status(f"Running {version_flag}")
     set_Run(True)
